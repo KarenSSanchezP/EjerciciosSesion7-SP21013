dc757839f0231dedc59f146899af2bfddd6d4b1d
Karen S. Sánchez <112920282+KarenSSanchezP@users.noreply.github.com>
2026-02-13 16:01:05 -0600

    add: Ejercicio 3 resuelto

---
+++ ejercicio3.py
@@ -0,0 +1,12 @@
+def conteo_caracteres(cadena):
+    conteo = {}
+    for caracter in cadena:
+        if caracter in conteo:
+            conteo[caracter] += 1
+        else:
+            conteo[caracter] = 1
+    return conteo
+
+cadena = input("Ingrese una cadena de texto: ")
+resultado = conteo_caracteres(cadena)
+print(f"El conteo de caracteres de la cadena '{cadena}' es: \n{resultado}")
